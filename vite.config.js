6bc540fb3f0086eeaf4aeeeeed57ac62dd7e227e
poshpinkmarketing <hello@poshpinkmarketing.com>
2026-05-27 17:09:12 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['jspdf']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/jspdf/, /node_modules/]
+    }
+  }
 })
